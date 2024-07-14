398bad73d8990bf8a61ac1ec1ddd9358b45ae74c
sosotechnologies <sosotech2000@gmail.com>
2024-07-14 07:56:29 -0400

    test

--- build-push-to-ecr/bump_version.sh
+++ build-push-to-ecr/bump_version.sh
@@ -14,8 +14,10 @@ new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 # Write the new version back to the VERSION file
 echo $new_version > VERSION
 
-sed -i "s/value: \"[0-9.]*\"/value: \"$new_version\"/g" build-push-to-gh/6-cheatsheet.yaml
-sed -i "s/value: \"[0-9.]*\"/value: \"$new_version\"/g" build-push-to-gh/WorkflowTemplate.yaml
+# Update WorkflowTemplate.yaml
+sed -i "s/value: \"[0-9.]*\"/value: \"$new_version\"/g" build-push-to-ecr/WorkflowTemplate.yaml
 
+# Update 6-cheatsheet.yaml
+sed -i "s/value: \"[0-9.]*\"/value: \"$new_version\"/g" build-push-to-ecr/6-cheatsheet.yaml
 
 echo $new_version
