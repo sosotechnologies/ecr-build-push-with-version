97e27246846995f912ffe3f6560d2dffb9da40d7
sosotechnologies <sosotech2000@gmail.com>
2024-07-15 20:19:31 -0400

    d

--- world/bump_version_world_template.sh
+++ world/bump_version_world_template.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Check if VERSION file exists
-if [ ! -f VERSION ]; then
-  echo "2.2.19" > VERSION
+# Check if VERSION-TEMP-WORLD file exists
+if [ ! -f VERSION-TEMP-WORLD ]; then
+  echo "2.2.19" > VERSION-TEMP-WORLD
 fi
 
-current_version=$(cat VERSION)
+current_version=$(cat VERSION-TEMP-WORLD)
 
 IFS='.' read -r -a version_parts <<< "$current_version"
 version_parts[2]=$((version_parts[2] + 1))
 new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 
-# Write the new version back to the VERSION file
-echo $new_version > VERSION
+# Write the new version back to the VERSION-TEMP-WORLD file
+echo $new_version > VERSION-TEMP-WORLD
 
 sed -i "s/value: \"[0-9.]*\"/value: \"$new_version\"/g" build-push-to-gh/WorkflowTemplate.yaml
 
--- world/bump_version_world_workflow.sh
+++ world/bump_version_world_workflow.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Check if VERSION file exists
-if [ ! -f VERSION-WORLD ]; then
-  echo "2.2.19" > VERSION-WORLD
+if [ ! -f VERSION-WF-WORLD ]; then
+  echo "2.2.19" > VERSION-WF-WORLD
 fi
 
-current_version=$(cat VERSION-WORLD)
+current_version=$(cat VERSION-WF-WORLD)
 
 IFS='.' read -r -a version_parts <<< "$current_version"
 version_parts[2]=$((version_parts[2] + 1))
 new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 
 # Write the new version back to the VERSION file
-echo $new_version > VERSION-WORLD
+echo $new_version > VERSION-WF-WORLD
 
 sed -i "s/value: \"[0-9.]*\"/value: \"$new_version\"/g" build-push-to-gh/worldworkflow.yaml
 
